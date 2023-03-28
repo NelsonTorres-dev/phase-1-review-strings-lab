b0fe1be7e7a094f769fc3a076032092931018329
Nelson Torres <ntorres381@gmail.com>
2023-03-28 16:14:02 -0700

    interpolates variables js expressions

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+
+
+const currentUser = 'Nelson Dev';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+
+
+
