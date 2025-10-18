896e1281e2fd9676a2086927acacb5bfe5fe9397
Nelson Torres <ntorres381@gmail.com>
2025-10-18 15:01:24 -0700

    template literals & interpolation

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+// const currentUser = 'Milan Torres'
+
+// const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+// const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+// const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`
+
+
+const currentUser = 'Milan Torres'
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`
